714bd87f09e5de76723d000dabba2d0f7b4faab2
Egis <e.gecius@gmail.com>
2016-04-03 07:06:24 +0100

    tidying

--- app/src/main/java/com/egecius/demo_designpatterns/abstract_factory/interfaces/Pizza.java
+++ app/src/main/java/com/egecius/demo_designpatterns/abstract_factory/interfaces/Pizza.java
@@ -1,10 +1,5 @@
 package com.egecius.demo_designpatterns.abstract_factory.interfaces;
 
-/**
- * Single Responsibility:
- *
- * //todo
- */
 public interface Pizza {
 
 	String getDescription();
--- app/src/main/java/com/egecius/demo_designpatterns/abstract_factory/interfaces/PizzaStore.java
+++ app/src/main/java/com/egecius/demo_designpatterns/abstract_factory/interfaces/PizzaStore.java
@@ -1,10 +1,6 @@
 package com.egecius.demo_designpatterns.abstract_factory.interfaces;
 
-/**
- * Single Responsibility:
- *
- * //todo
- */
+
 public interface PizzaStore {
 
 	Pizza getPizza(String pizzaName);
